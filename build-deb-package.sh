badd2ac30af0feea882c35aad1cb122409bd5097
Tim Abell <tim.abell@timwise.co.uk>
2008-12-16 14:31:58 +0000

    copy compiled binary, verbose script output, ignore output folder

--- build-deb-package.sh
+++ build-deb-package.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -v
 
 if [ -d package ]; then
 	rm -rf package #harsh but effective. feel free to improve on this
@@ -12,4 +12,6 @@ mkdir -p package/usr/share/doc/dargui
 cp changelog docs/* package/usr/share/doc/dargui/
 mkdir -p package/usr/share/man/man1
 gzip -c docs/man/dargui.1 > package/usr/share/man/man1/dargui.1.gz
-
+echo '2.0' > package/debian-binary
+#you need to have the project compiled into folder 'compiled' for this bit to work.
+cp compiled/dargui package/usr/bin/
