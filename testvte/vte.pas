9cb8b6a25faceeaa2e2c09ebadeac42747a5c8da
mgpoole <mgpoole@5a7143a4-a040-0410-86c7-d5f9f512a94d>
2011-05-12 09:17:13 +0000

    Added licensing notices
    git-svn-id: https://dargui.svn.sourceforge.net/svnroot/dargui@307 5a7143a4-a040-0410-86c7-d5f9f512a94d

--- testvte/vte.pas
+++ testvte/vte.pas
@@ -1,3 +1,29 @@
+{*
+ * vte.pas
+ *
+ * This unit provides pascal bindings for libvte9
+ * Author: Malcolm Poole <mgpoole@users.sourceforge.net>
+ * 12 May 2011
+ *}
+
+{*
+ * libvte is Copyright (C) 2001,2002,2003,2009,2010 Red Hat, Inc.
+ *
+ * This is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *}
+
 unit vte;
 interface
 uses glib2, pango, gdk2, gtk2, gdk2pixbuf;
--- testvte/vteterminal.pas
+++ testvte/vteterminal.pas
@@ -1,3 +1,25 @@
+{
+  VTETerminal.pas
+
+  Classes for providing GTK terminal shells in pascal applications,
+  using libvte9.
+
+  Copyright (C) 2011 Malcolm Poole <mgpoole@users.sourceforge.net>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+  for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+}
 unit VTETerminal;
 
 {$mode objfpc}{$H+}
@@ -217,7 +239,7 @@ implementation
 { TCustomVTETerminal callbacks }
 
 { NOTE: This function does not work with libvte v0.16 [ < 0.20 ? ] }
-procedure CustomVTE_child_exit_callback( Term: TObject);
+procedure CustomVTE_child_exit_callback( Term: TObject); cdecl;
 begin
   if Term is TCustomVTETerminal then TCustomVTETerminal(Term).DoProcessExit;
 end;
